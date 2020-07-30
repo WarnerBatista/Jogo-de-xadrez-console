cf23a5e9e1b7f72d017548a6f6e8ac76f2c566c5
Warner Batista dos Santos <warnerbatista@gmail.com>
2020-07-30 18:04:53 -0300

    Acrescentando as demais pecas

--- Xadrez_Console/xadrez/Bispo.cs
+++ Xadrez_Console/xadrez/Bispo.cs
@@ -2,16 +2,22 @@
 
 namespace xadrez
 {
-    class Torre : Peca
+    class Bispo : Peca
     {
-        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor)
         {
-
+            
         }
 
         public override string ToString()
         {
-            return "T";
+            return "B";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
         }
 
         public override bool[,] movimentospossiveis()
@@ -19,20 +25,8 @@ namespace xadrez
             bool[,] mat = new bool[tab.linhas, tab.colunas];
             Posicao pos = new Posicao(0, 0);
 
-            //Acima
-            pos.definirValores(posicao.Linha - 1, posicao.Coluna);
-            while(tab.posicaoValida(pos) && podeMover(pos))
-            {
-                mat[pos.Linha, pos.Coluna] = true;
-                if(tab.peca(pos)!= null && tab.peca(pos).cor != cor)
-                {
-                    break;
-                }
-                pos.Linha = pos.Linha - 1;
-            }
-
-            //Abaixo
-            pos.definirValores(posicao.Linha + 1, posicao.Coluna);
+            //Noroeste
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna -1);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -40,11 +34,11 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Linha = pos.Linha + 1;
+                pos.definirValores(pos.Linha - 1, pos.Coluna - 1);
             }
 
-            //Direita
-            pos.definirValores(posicao.Linha, posicao.Coluna + 1);
+            //Nordeste
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna + 1);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -52,11 +46,11 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Coluna = pos.Coluna + 1;
+                pos.definirValores(pos.Linha - 1, pos.Coluna + 1);
             }
 
-            //Esquerda
-            pos.definirValores(posicao.Linha, posicao.Coluna - 1);
+            //Suldoeste
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna - 1);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -64,15 +58,21 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Coluna = pos.Coluna - 1;
+                pos.definirValores(pos.Linha + 1, pos.Coluna - 1);
+            }
+
+            //Sudeste
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna + 1);
             }
             return mat;
-        }
-
-        private bool podeMover(Posicao pos)
-        {
-            Peca p = tab.peca(pos);
-            return p == null || p.cor != cor;
         }
     }
 }
